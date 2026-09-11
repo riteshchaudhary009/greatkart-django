c7ea81dc2af3397507a1f8f789992d4e86b911ee
ritesh chaudhary <chudharyritesh2078@gmail.com>
2026-09-11 15:49:50 +0545

    added new commit

--- carts/views.py
+++ carts/views.py
@@ -66,6 +66,8 @@ def remove_cart_item(request, product_id):
     
 
 def cart(request, total=0, quantity=0, cart_items=None):
+    tax=0
+    grand_total=0
 
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
